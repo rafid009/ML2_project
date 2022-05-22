433b0127f512193c276167c449c0c8c33a77d9f0
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2022-05-22 16:51:30 -0700

    1

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def mask_out_nan(output, target):
 def get_visit_likelihood(d, y):
     mask = ~torch.isnan(y)
     y_t = torch.nan_to_num(y)
-    print(f"d: {d.shape} y_t: {y_t.shape}")
+    print(f"d: {d.shape} y_t: {y_t.shape} y: {y.shape}")
     l = torch.pow(d, y_t) * torch.pow((1 - d), (1 - y_t))
     return l * mask, y_t
 
